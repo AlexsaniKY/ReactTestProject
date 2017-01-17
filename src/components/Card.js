5e9a02f92d8ec54705b2f1b0d91c6f3ce6e4e9d5
AlexsaniKY <cerafem@gmail.com>
2017-01-16 23:59:32 -0500

    styled card title to the left with border

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,10 @@ class Card extends Component {
     render() {
         return (
             <div className="Card">
+                <span className="Card-title">
+                    {this.props.title}
+                </span>
                 <div className="Card-innerCard">
-                    <span className="Card-title">
-                        {this.props.title}
-                    </span>
                     {this.props.children}
                 </div>
             </div>
